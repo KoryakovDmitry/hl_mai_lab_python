3af93e5bb99ec2d45b17966d65c90752a04c3f16
dimitrii <dm.koryakov@gmail.com>
2023-11-30 03:08:33 +0100

    [lab2] upd model

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,9 +9,9 @@ from sqlalchemy.orm import sessionmaker
 # Load environment variables
 load_dotenv()
 
-# Database connection details
+# These should now point to the ProxySQL service
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", 3360)
+DB_PORT = os.getenv("DB_PORT", 6033)  # Port changed to ProxySQL's client port
 DB_USER = os.getenv("DB_USER", "stud")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "stud")
 DB_DATABASE = os.getenv("DB_DATABASE", "archdb")
@@ -20,6 +20,7 @@ DB_DATABASE = os.getenv("DB_DATABASE", "archdb")
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 )
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
